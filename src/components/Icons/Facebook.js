d28336e434169325376a889bf5bf5b046ba4f3ed
Santiago Spinetto Jung <sanjs965@gmail.com>
2019-05-09 11:54:06 -0300

    styles

--- src/components/Icons/Facebook.js
+++ src/components/Icons/Facebook.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SvgFacebook = props => (
   <svg viewBox="0 0 24 24" {...props}>
-    <path d="M19 4v3h-2a1 1 0 0 0-1 1v2h3v3h-3v7h-3v-7h-2v-3h2V7.5C13 5.56 14.57 4 16.5 4M20 2H4a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2z" />
+    <path d="M0 0v24h24V0H0zm16 7h-1.923C13.461 7 13 7.252 13 7.889V9h3l-.239 3H13v8h-3v-8H8V9h2V7.077C10 5.055 11.064 4 13.461 4H16v3z" />
   </svg>
 );
 
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import FacebookIcon from '../Icons/Facebook'
 import Wapp from '../Icons/Wapp'
+import PlaceholderNew from '../Icons/PlaceholderNew'
 import '../../assets/css/svg.css'
 import '../../assets/css/footer.min.css'
 
@@ -9,17 +10,21 @@ class Footer extends Component{
     return(
       <div className="footer">
          <ul >
+            <li> Telefonos: 03547-429157 / 03547-15510209
+                <a className="c-link c-link--wapp c-tooltip" aria-label="WhatsApp">
+                    <Wapp className="c-icon"/>
+                </a>
+            </li><br/>
             <li>Encontranos en Facebook
-                <a className="c-link c-link--facebook c-tooltip" aria-label="Facebook" href="https://www.facebook.com/Departamentosarrayanes/">
+                 <a className="c-link c-link--facebook c-tooltip" aria-label="Facebook" href="https://www.facebook.com/Departamentosarrayanes/" target="_blank">
                     <FacebookIcon className="c-icon" />
                 </a>
             </li><br/>
-            <li>Telefonos: 03547-429157 / 03547-15510209
-                <a className="c-link c-link--wapp c-tooltip" aria-label="WhatsApp">
-                    <Wapp className="c-wapp"/>
+            <li>Direccion: ViceInt.Doctor Jorge Nahal 235
+                <a className="c-link c-link--map c-tooltip" aria-label="Ubicación">
+                     <PlaceholderNew className="c-icon"/>
                 </a>
-            </li><br/>
-            <li>Direccion: ViceInt.Doctor Jorge Nahal 235</li>
+            </li>
          </ul>
       </div>
     )
